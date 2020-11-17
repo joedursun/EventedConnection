b402aa97eb87bf9beef46a0dfb5dda1c7cfdb0f7
Joe Dursun <joedursun@gmail.com>
2020-11-17 16:07:39 -0500

    Add event channels and mutex

--- connection/stableConnection.go
+++ connection/stableConnection.go
@@ -12,12 +12,19 @@ const TCPReadTimeout = 10 * time.Minute
 const readBufferSize = 16 * 1024 // 16 KB
 
 // StableConnection gives us a stable way to connect and maintain a connection to a TCP endpoint.
+// StableConnection broadcasts 3 separate events via closing a channel: Connected, Disconnected, and Canceled.
+// This allows any number of downstream consumers to be informed when a state change happens.
 type StableConnection struct {
   C                    net.Conn
   ConnectionTimeout    int
   Endpoint             string
   Read                 chan []byte
+  Disconnected         chan struct{}
+  Connected            chan struct{}
+  Canceled             chan struct{}
+
   writeChan            chan []byte
+  mutex                *sync.RWMutex // allows for using this connection in multiple goroutines
   active               bool
 }
 
@@ -27,12 +34,19 @@ func NewStableConnection(conf *Config, endpoint string) (*StableConnection, erro
   if len(endpoint) == 0 {
     return nil, errors.New("Invalid endpoint (empty string)")
   }
+
   conn.Endpoint = endpoint
   conn.ConnectionTimeout = 5 // default timeout for connecting
   if conf.Timeout > 0 {
     conn.ConnectionTimeout = conf.Timeout
   }
+
+  conn.Disconnected = make(chan struct{}, 0)
+  conn.Connected = make(chan struct{}, 0)
+  conn.Canceled = make(chan struct{}, 0)
   conn.Read = make(chan []byte, 5) // buffer of 5 packets (up to 5 * readBufferSize). reduces blocking when reading from connection
+  conn.writeChan = make(chan []byte)
+  conn.mutex = &sync.RWMutex{}
   conn.active = false
   return &conn, nil
 }
