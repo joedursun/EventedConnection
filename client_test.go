dcaf8dcfb279f01301057312285f1a4c9270375c
Joe Dursun <joedursun@gmail.com>
2020-11-29 09:43:40 -0500

    Add tests for Client defaults

--- client_test.go
+++ client_test.go
@@ -3,9 +3,10 @@ package eventedconnection_test
 import (
 	. "github.com/joedursun/EventedConnection"
 	"testing"
+	"time"
 )
 
-func TestNewClient(t *testing.T) {
+func TestNewClient_ConfigDefaults(t *testing.T) {
 	emptyConf := Config{}
 	con, err := NewClient(&emptyConf)
 	if con != nil {
@@ -14,5 +15,19 @@ func TestNewClient(t *testing.T) {
 
 	if err == nil {
 		t.Error("Expected err to be of type error but got nil")
+	}
+
+	conf := Config{Endpoint: "localhost:5555"}
+	con, err = NewClient(&conf)
+	assertEqual(t, con.Endpoint, conf.Endpoint)
+	assertEqual(t, con.ConnectionTimeout, 30*time.Second)
+	assertEqual(t, con.ReadTimeout, 1*time.Hour)
+	assertEqual(t, con.WriteTimeout, 5*time.Second)
+	assertEqual(t, con.ReadBufferSize, 16*1024)
+}
+
+func assertEqual(t *testing.T, a interface{}, b interface{}) {
+	if a != b {
+		t.Fatalf("%s != %s", a, b)
 	}
 }
